e5a81d1702ba37c2597a1ef6a65d023358edf454
ignaac1o <ignacioalmodovarcardenas111@gmail.com>
2021-12-09 10:46:44 +0100

    Create Test.R
    Preprocess Data

---
+++ Test.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(caret)
+
+
+getwd()
+setwd("/Users/ignacioalmodovarcardenas/Desktop/Statistical Learning/Statistical_Learning/")
+
+data=read.csv2("TravelInsurancePrediction.csv",sep=",")
+
+## PREPROCESS DATA
+## https://www.section.io/engineering-education/data-preprocessing-in-r/
+
+## 1
+#We check for missing values
+cbind(lapply(lapply(data, is.na), sum))
+#As we do not have anny missing values we do not have to do anything in this step.
+
+
+## 2
+#Encode categorical data
+
+data$Employment.Type %>% factor(levels = c("Government Sector","Private Sector/Self Employed"),
+                                  labels = c(1,2))
+
+data$GraduateOrNot %>% factor(levels = c("Yes","No"),labels = c(1,0))
+data$FrequentFlyer %>% factor(levels = c("Yes","No"),labels = c(1,0))
+data$EverTravelledAbroad %>% factor(levels = c("Yes","No"),labels = c(1,0))
+
+data$FamilyMembers=as.factor(data$FamilyMembers)
+
+## 3 
+# Split data into Training and Test
+index=createDataPartition(y=data$Employment.Type,times = 1,p=0.8,list = FALSE)
+
+trainSet=data[index,]
+testSet=data[-index,]
+
+## 4
+# Standardize continuous data
+
+trainSet[,c(2,5)] %>% scale()
+testSet[,c(2,5)] %>% scale()
+
+
+
+
+
+
